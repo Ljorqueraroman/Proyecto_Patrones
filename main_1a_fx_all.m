b407a4b64118a532929052e868bc61a044543816
Luis Alfonso Jorquera Roman <lajorquera@uc.cl>
2017-06-19 20:31:10 -0400

    Added back commented code

--- main_1a_fx_all.m
+++ main_1a_fx_all.m
@@ -10,7 +10,7 @@ disp('Extracting features from the whole set...')
 [ X_raw, Xn_raw, d_raw, id_raw ] = ...
   fx_extract_set(range , @dm_name_function, cfg.loc , cfg.seg_method);
 
-%if (config.wanna_save)
-%  save( sav.fx_all, 'X_raw', 'Xn_raw', 'd_raw','id_raw');
-%  disp(['Saved training features to ' sav.fx_all]);
-%end
+if (config.wanna_save)
+  save( sav.fx_all, 'X_raw', 'Xn_raw', 'd_raw','id_raw');
+  disp(['Saved training features to ' sav.fx_all]);
+end
